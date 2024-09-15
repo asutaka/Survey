8df71880f9b3ae8d7f0447f503346be07190d12c
Nguyễn Văn Phú <asutaka@outlook.com>
2024-09-15 16:09:14 +0700

    update code

--- StockLib/DAL/Entity/StockType.cs
+++ StockLib/DAL/Entity/StockType.cs
@@ -7,10 +7,10 @@ namespace StockLib.DAL.Entity
     {
         public string s { get; set; }//symbol
         public int ty1 { get; set; }
-        public int ty1_r { get; set; }
         public int ty2 { get; set; }
-        public int ty2_r { get; set; }
         public int ty3 { get; set; }
+        public int ty1_r { get; set; }
+        public int ty2_r { get; set; }
         public int ty3_r { get; set; }
     }
 }
