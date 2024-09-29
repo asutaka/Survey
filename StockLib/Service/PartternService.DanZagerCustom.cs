e616a7c882d24dc5b983ed80f0873816050246e1
phunv@ftech.ai <phunv@ftech.ai>
2024-09-30 00:25:52 +0700

    update code

--- StockLib/Service/PartternService.DanZagerCustom.cs
+++ StockLib/Service/PartternService.DanZagerCustom.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
 using Skender.Stock.Indicators;
+using StockLib.DAL.Entity;
 using StockLib.Utils;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using static iTextSharp.text.pdf.AcroFields;
 
 namespace StockLib.Service
 {
@@ -24,6 +28,20 @@ namespace StockLib.Service
 
                 //var lData = await _apiService.SSI_GetDataStock_Alltime(code);
                 var lData = await _apiService.SSI_GetDataStock(code);
+
+                ////test
+                //var count = lData.Count();
+                //var lTmp = lData.Take(50).ToList();
+                //for (int i = 50; i < count - 1; i++) 
+                //{
+                //    var check = lTmp.CheckDanZangerCustom(a);
+                //    if (check)
+                //    {
+                //        Console.WriteLine($"BUY: {lTmp.Last().Date.ToString("dd/MM/yyyy")}");
+                //    }
+                //    lTmp.Add(lData.ElementAt(i));
+                //}
+                ////
                 DanZagerCustom(lData, a);
             }
             catch (Exception ex)
@@ -109,6 +127,47 @@ namespace StockLib.Service
             {
                 _logger.LogError($"PartternService.DanZagerCustom|EXCEPTION| {ex.Message}");
             }
+        }
+    }
+
+    public static class clsDanZangerCustom
+    {
+        public static bool CheckDanZangerCustom(this List<Quote> lVal, decimal a)
+        {
+            try
+            {
+                if (!lVal.Any())
+                    return false;
+                var count = lVal.Count();
+                var lbb = lVal.GetBollingerBands();
+                var item = lVal.Last();
+                var near = lVal.SkipLast(1).TakeLast(1).First();
+                var bb = lbb.Last();
+                var bb_near = lbb.SkipLast(1).TakeLast(1).First();
+                if (item.Close < item.Open * (decimal)1.01
+                        || item.Low >= (decimal)bb.Sma)
+                    return false;
+
+                var vol_check = lVal.Skip(count - 11).Take(10).Count(x => item.Volume >= x.Volume * (decimal)1.07) >= 9;
+                if (!vol_check)
+                    return false;
+
+                var bb_check = (item.Low < (decimal)bb.Sma && item.Close > (decimal)bb.Sma && item.High < (decimal)bb.UpperBand && item.Close < item.Open * ((decimal)1 + Math.Round(a / 200, 2)))
+                            || (item.Low < (decimal)bb.LowerBand && item.Close > (decimal)bb.LowerBand && item.High < (decimal)bb.Sma);
+                if (!bb_check)
+                    return false;
+
+                var last_check = near.Close < (decimal)bb_near.Sma;
+                if (!last_check)
+                    return false;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"PartternService.CheckDanZangerCustom|EXCEPTION| {ex.Message}");
+            }
+            return false;
         }
     }
 }
--- StockTestCase/App.cs
+++ StockTestCase/App.cs
@@ -25,17 +25,17 @@ namespace StockTestCase
             //await _service.SurveyGoldFish("DC4");
             //await _service.SurveySuperTrend("VNINDEX");
             //await _service.SurveyVCP("NTL");
-            //await _service.SurveyDanZagerCustom("TCH");
+            await _service.SurveyDanZagerCustom("TCH");
             //await _service.Survey3C("DPG");
 
             //////#region Test All Ma Chung Khoan
-            foreach (var stock in StaticVal._lStock.Where(x => x.rank == 1 || x.rank == 2))
-            {
-                await _service.SurveyDanZagerCustom(stock.s);
-                Thread.Sleep(1000);
-            }
+            //foreach (var stock in StaticVal._lStock.Where(x => x.rank == 1 || x.rank == 2))
+            //{
+            //    await _service.SurveyDanZagerCustom(stock.s);
+            //    Thread.Sleep(1000);
+            //}
 
-            _service.RankChungKhoan();
+            //_service.RankChungKhoan();
             //_service.TotalDays();
             //////#endregion
         }
