c544050a92c73bc28ac806e7754666cd0e787f71
Nguyễn Văn Phú <phunv@ftech.ai>
2024-10-02 17:28:07 +0700

    update code

--- StockLib/DAL/Settings/RegisterDAL.cs
+++ StockLib/DAL/Settings/RegisterDAL.cs
@@ -36,7 +36,6 @@ namespace StockLib.DAL.Settings
             services.AddSingleton<IThongKeHaiQuanRepo, ThongKeHaiQuanRepo>();
             services.AddSingleton<IChiSoPERepo, ChiSoPERepo>();
             services.AddSingleton<IKeHoachRepo, KeHoachRepo>();
-            services.AddSingleton<IShareRepo, ShareRepo>();
             services.AddSingleton<ISpecialInfoRepo, SpecialInfoRepo>();
         }
     }
