9c64e001e5413fe37876ff2d3f322554b4ae6d99
Nguyễn Văn Phú <asutaka@outlook.com>
2024-10-04 23:03:28 +0700

    update code

--- StockLib/Service/TeleService.Private.cs
+++ StockLib/Service/TeleService.Private.cs
@@ -25,6 +25,25 @@ namespace StockLib.Service
             return _bot;
         }
 
+        private async Task PrintImage(string code, long userId, bool isNganh)
+        {
+            var curDirectory = Directory.GetCurrentDirectory();
+            var path = string.Empty;
+            if(isNganh)
+            {
+                path = $"{curDirectory}/Resource/Nganh/{code}.png";
+            }
+            else
+            {
+                path = $"{curDirectory}/Resource/{code}.png";
+            }
+            if (System.IO.File.Exists(path))
+            {
+                var file = new FileStream(path, FileMode.Open);
+                await BotInstance().SendPhotoAsync(userId, InputFile.FromStream(file));
+            }
+        }
+
         private async Task Analyze(long userId, string input)
         {
             var output = new StringBuilder();
@@ -40,115 +59,148 @@ namespace StockLib.Service
                 input = input.Replace("[", "").Replace("]", "").Replace("*", "").Replace("@", "");
                 if (StaticVal._lBanLeKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Bán Lẻ
                 {
+                    await PrintImage(EStockType.BanLe.ToString(), userId, true);
                     await NganhBanLe(userId);
                     return;
                 }
                 if (StaticVal._lBatDongSanKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Bất động sản
                 {
+                    await PrintImage(EStockType.BDS.ToString(), userId, true);
                     await NganhBatDongSan(userId);
                     return;
                 }
                 if (StaticVal._lCangBienKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Cảng biển
                 {
+                    await PrintImage(EStockType.CangBien.ToString(), userId, true);
                     await NganhCangBien(userId);
                     return;
                 }
                 if (StaticVal._lCaoSuKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Cao Su
                 {
+                    await PrintImage(EStockType.CaoSu.ToString(), userId, true);
                     await NganhCaoSu(userId);
                     return;
                 }
                 if (StaticVal._lChungKhoanKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Chứng khoán
                 {
+                    await PrintImage(EStockType.ChungKhoan.ToString(), userId, true);
                     await NganhChungKhoan(userId);
                     return;
                 }
                 if (StaticVal._lDetMayKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Dệt may
                 {
+                    await PrintImage(EStockType.DetMay.ToString(), userId, true);
                     await NganhDetMay(userId);
                     return;
                 }
                 if (StaticVal._lDienKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Điện
                 {
+                    await PrintImage(EStockType.NangLuong.ToString(), userId, true);
                     await NganhDien(userId);
                     return;
                 }
                 if (StaticVal._lGoKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Gỗ
                 {
+                    await PrintImage(EStockType.Go.ToString(), userId, true);
                     await NganhGo(userId);
                     return;
                 }
                 if (StaticVal._lHangKhongKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Hàng không
                 {
+                    await PrintImage(EStockType.HangKhong.ToString(), userId, true);
                     await NganhHangKhong(userId);
                     return;
                 }
                 if (StaticVal._lLogisticKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Logistic
                 {
+                    await PrintImage(EStockType.VanTaiBien.ToString(), userId, true);
                     await NganhLogistic(userId);
                     return;
                 }
                 if (StaticVal._lNganHangKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Ngân Hàng
                 {
+                    await PrintImage(EStockType.NganHang.ToString(), userId, true);
                     await NganhNganHang(userId);
                     return;
                 }
                 if (StaticVal._lNhuaKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Nhựa
                 {
+                    await PrintImage(EStockType.Nhua.ToString(), userId, true);
                     await NganhNhua(userId);
                     return;
                 }
                 if (StaticVal._lOtoKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ô tô
                 {
+                    await PrintImage(EStockType.Oto.ToString(), userId, true);
                     await NganhOto(userId);
                     return;
                 }
                 if (StaticVal._lPhanBonKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Phân bón, hóa chất
                 {
+                    await PrintImage(EStockType.PhanBon.ToString(), userId, true);
                     await NganhPhanBon(userId);
                     return;
                 }
                 if (StaticVal._lThanKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Than
                 {
+                    await PrintImage(EStockType.Than.ToString(), userId, true);
                     await NganhThan(userId);
                     return;
                 }
                 if (StaticVal._lThepKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Thép
                 {
+                    await PrintImage(EStockType.Thep.ToString(), userId, true);
                     await NganhThep(userId);
                     return;
                 }
                 if (StaticVal._lThuySanKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Thủy sản
                 {
+                    await PrintImage(EStockType.ThuySan.ToString(), userId, true);
                     await NganhThuySan(userId);
                     return;
                 }
                 if (StaticVal._lXimangKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Xi măng
                 {
+                    await PrintImage(EStockType.XiMang.ToString(), userId, true);
                     await NganhXimang(userId);
                     return;
                 }
                 if (StaticVal._lXayDungKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Xây dựng
                 {
+                    await PrintImage(EStockType.XayDung.ToString(), userId, true);
                     await NganhXayDung(userId);
                     return;
                 }
 
                 if (StaticVal._lKCNKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành KCN
                 {
+                    await PrintImage(EStockType.KCN.ToString(), userId, true);
                     await NganhKCN(userId);
                     return;
                 }
 
                 if (StaticVal._lDauKhiKey.Any(x => x.RemoveSpace().ToUpper().Equals(input)))//Ngành Dầu Khí
                 {
+                    await PrintImage(EStockType.DauKhi.ToString(), userId, true);
                     await NganhDauKhi(userId);
                     return;
                 }
             }
             else if(input.Length == 3) //Mã chứng khoán
             {
+                await PrintImage(input.ToUpper(), userId, false);
                 await MaChungKhoan(userId, input);
+            }
+            else if(input.Equals("test", StringComparison.OrdinalIgnoreCase))
+            {
+                var curDirectory = Directory.GetCurrentDirectory();
+                var path = $"{curDirectory}/Resource/BAF.png";
+                if(System.IO.File.Exists(path))
+                {
+                    var file = new FileStream(path, FileMode.Open);
+                    await BotInstance().SendPhotoAsync(userId, InputFile.FromStream(file));
+                }
+               
             }
 
             //var entityStock = _lStock.FirstOrDefault(x => x.s.Equals(input.ToUpper()));
