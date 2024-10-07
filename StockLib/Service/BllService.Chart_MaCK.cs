54b98f3232e1c39ece0e8a4be7900b9fbf6f94c9
Nguyễn Văn Phú <asutaka@outlook.com>
2024-10-07 22:24:35 +0700

    update code

--- StockLib/Service/BllService.Chart_MaCK.cs
+++ StockLib/Service/BllService.Chart_MaCK.cs
@@ -10,12 +10,12 @@ namespace StockLib.Service
             if (stock == null)
                 return null;
 
-            var isXayDung = stock.cat.Any(y => y.ty == (int)EStockType.XayDung);
+            var isXayDung = stock.cat.First().ty == (int)EStockType.XayDung;
             if (isXayDung) 
             {
                 return await Chart_XayDung(input);
             }
-            var isKCN = stock.cat.Any(y => y.ty == (int)EStockType.KCN);
+            var isKCN = stock.cat.First().ty == (int)EStockType.KCN;
             if (isKCN) 
             {
                 return await Chart_KCN(input);
@@ -25,27 +25,27 @@ namespace StockLib.Service
             {
                 return await Chart_BatDongSan(input);
             }
-            var isBDS = stock.cat.Any(y => y.ty == (int)EStockType.BDS);
+            var isBDS = stock.cat.First().ty == (int)EStockType.BDS;
             if (isBDS) 
             {
                 return await Chart_BatDongSan(input);
             }
-            var isNganHang = stock.cat.Any(y => y.ty == (int)EStockType.NganHang);
+            var isNganHang = stock.cat.First().ty == (int)EStockType.NganHang;
             if (isNganHang)
             {
                 return await Chart_NganHang(input);
             }
-            var isChungKhoan = stock.cat.Any(y => y.ty == (int)EStockType.ChungKhoan);
+            var isChungKhoan = stock.cat.First().ty == (int)EStockType.ChungKhoan;
             if (isChungKhoan)
             {
                 return await Chart_ChungKhoan(input);
             }
-            var isThep = stock.cat.Any(y => y.ty == (int)EStockType.Thep);
+            var isThep = stock.cat.First().ty == (int)EStockType.Thep;
             if (isThep)
             {
                 return await Chart_Thep(input);
             }
-            var isBanLe = stock.cat.Any(y => y.ty == (int)EStockType.BanLe);
+            var isBanLe = stock.cat.First().ty == (int)EStockType.BanLe;
             if (isBanLe)
             {
                 return await Chart_BanLe(input);
@@ -62,79 +62,81 @@ namespace StockLib.Service
                 return await Chart_Dien(input);
             }
 
-            var isCangBien = stock.cat.Any(y => y.ty == (int)EStockType.CangBien);
+            var isCangBien = stock.cat.First().ty == (int)EStockType.CangBien;
             if (isCangBien)
             {
                 return await Chart_CangBien(input);
             }
 
-            var isLogistic = stock.cat.Any(y => y.ty == (int)EStockType.Logistic || y.ty == (int)EStockType.VanTaiBien);
+            var isLogistic = stock.cat.First().ty == (int)EStockType.Logistic 
+                || stock.cat.First().ty == (int)EStockType.VanTaiBien;
             if (isLogistic)
             {
                 return await Chart_Logistic(input);
             }
 
-            var isHangKhong = stock.cat.Any(y => y.ty == (int)EStockType.HangKhong);
+            var isHangKhong = stock.cat.First().ty == (int)EStockType.HangKhong;
             if (isHangKhong)
             {
                 return await Chart_HangKhong(input);
             }
 
-            var isCaoSu = stock.cat.Any(y => y.ty == (int)EStockType.CaoSu);
+            var isCaoSu = stock.cat.First().ty == (int)EStockType.CaoSu;
             if (isCaoSu)
             {
                 return await Chart_CaoSu(input);
             }
 
-            var isNhua = stock.cat.Any(y => y.ty == (int)EStockType.Nhua);
+            var isNhua = stock.cat.First().ty == (int)EStockType.Nhua;
             if (isNhua)
             {
                 return await Chart_Nhua(input);
             }
 
-            var isOto = stock.cat.Any(y => y.ty == (int)EStockType.Oto || y.ty == (int)EStockType.OtoTai);
+            var isOto = stock.cat.First().ty == (int)EStockType.Oto 
+                || stock.cat.First().ty == (int)EStockType.OtoTai;
             if (isOto)
             {
                 return await Chart_Oto(input);
             }
 
-            var isPhanBon = stock.cat.Any(y => y.ty == (int)EStockType.PhanBon);
+            var isPhanBon = stock.cat.First().ty == (int)EStockType.PhanBon;
             if (isPhanBon)
             {
                 return await Chart_PhanBon(input);
             }
 
-            var isThan = stock.cat.Any(y => y.ty == (int)EStockType.Than);
+            var isThan = stock.cat.First().ty == (int)EStockType.Than;
             if (isThan)
             {
                 return await Chart_Than(input);
             }
 
-            var isThuySan = stock.cat.Any(y => y.ty == (int)EStockType.ThuySan);
+            var isThuySan = stock.cat.First().ty == (int)EStockType.ThuySan;
             if (isThuySan)
             {
                 return await Chart_ThuySan(input);
             }
 
-            var isXimang = stock.cat.Any(y => y.ty == (int)EStockType.XiMang);
+            var isXimang = stock.cat.First().ty == (int)EStockType.XiMang;
             if (isXimang)
             {
                 return await Chart_Ximang(input);
             }
 
-            var isDetmay = stock.cat.Any(y => y.ty == (int)EStockType.DetMay);
+            var isDetmay = stock.cat.First().ty == (int)EStockType.DetMay;
             if (isDetmay)
             {
                 return await Chart_DetMay(input);
             }
 
-            var isGo = stock.cat.Any(y => y.ty == (int)EStockType.Go);
+            var isGo = stock.cat.First().ty == (int)EStockType.Go;
             if (isGo)
             {
                 return await Chart_Go(input);
             }
 
-            var isDauKhi = stock.cat.Any(y => y.ty == (int)EStockType.DauKhi);
+            var isDauKhi = stock.cat.First().ty == (int)EStockType.DauKhi;
             if (isDauKhi)
             {
                 return await Chart_DauKhi(input);
