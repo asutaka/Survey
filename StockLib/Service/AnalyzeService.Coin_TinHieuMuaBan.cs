79dee4903d39dce7fe71ff514615b06e53b351e8
Nguyễn Văn Phú <phunv@ftech.ai>
2024-10-30 11:45:49 +0700

    update code

--- StockLib/Service/AnalyzeService.Coin_TinHieuMuaBan.cs
+++ StockLib/Service/AnalyzeService.Coin_TinHieuMuaBan.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using Skender.Stock.Indicators;
-using StockLib.DAL.Entity;
 using StockLib.Utils;
 using System.Text;
 
@@ -8,98 +6,51 @@ namespace StockLib.Service
 {
     public partial class AnalyzeService
     {
+        private const int _interval = 5;
+        private List<(int, string)> _lLong = new List<(int, string)>();
         public async Task<(int, string)> TinHieuMuaBanCoin()
         {
             try
             {
-                var lCoin = _coinRepo.GetAll();
-
-                var lDanZVolume = new List<Coin>();
-                var lSuperTrend = new List<Coin>();
-                var lSuperTrendPhrase2 = new List<Coin>();
-
-                foreach (var item in lCoin)
+                var lSymbols = await StaticVal.ByBitInstance().SpotApiV3.ExchangeData.GetSymbolsAsync();
+                var lSymbolFilter = lSymbols.Data.Where(x => x.QuoteAsset == "USDT");
+                var time = DateTimeOffset.Now.AddDays(-3).ToUnixTimeMilliseconds();
+                foreach (var item in lSymbolFilter)
                 {
-                    if (item is null
-                        || !item.indicator.Any())
-                        continue;
-
-                    var lByBit = await StaticVal.ByBitInstance().V5Api.ExchangeData.GetKlinesAsync(Bybit.Net.Enums.Category.Spot, item.s, Bybit.Net.Enums.KlineInterval.OneHour, null, null, 1000);
-                    var lData = lByBit.Data.List.Select(x => new Quote
+                    var lData = await _apiService.GetCoinData_Binance(item.Alias, $"{_interval}m", time);
+                    Thread.Sleep(200);
+                    var eliot = lData.CheckEliot();
+                    if(eliot.Item1)
                     {
-                        Date = x.StartTime,
-                        Open = x.OpenPrice,
-                        High = x.HighPrice,
-                        Low = x.LowPrice,
-                        Close = x.ClosePrice,
-                        Volume = x.Volume,
-                    }).ToList();
-
-                    if (item.indicator.Any(x => x.ty == (int)EIndicator.DanZangerVolumne))
-                    {
-                        var isDanz = lData.CheckDanZangerCustom(7);
-                        if (isDanz)
-                        {
-                            lDanZVolume.Add(item);
-                        }
+                        _lLong.Add((eliot.Item2, item.Alias));
                     }    
-                    
-                    if (item.indicator.Any(x => x.ty == (int)EIndicator.SuperTrend))
-                    {
-                        var isSuperTrend = lData.CheckSuperTrend();
-                        if (isSuperTrend)
-                        {
-                            lSuperTrend.Add(item);
-                        }
-                    }
-
-
-                    if (item.indicator.Any(x => x.ty == (int)EIndicator.SuperTrendPhrase2))
-                    {
-                        var isSuperTrendPhrase2 = lData.CheckSuperTrendPharse2();
-                        if (isSuperTrendPhrase2)
-                        {
-                            lSuperTrendPhrase2.Add(item);
-                        }
-                    }
                 }
 
                 var sBuilder = new StringBuilder();
 
 
-                if (lDanZVolume.Any())
+                if (_lLong.Any())
                 {
                     sBuilder.AppendLine();
-                    sBuilder.AppendLine("[Tín hiệu DanZVolume - COIN]");
+                    sBuilder.AppendLine("[Eliot sóng 3 - COIN]");
 
-                    foreach (var item in lDanZVolume.Take(10))
+                    var lBuy1 = _lLong.Where(x => x.Item1 == 1);
+                    var lBuy2 = _lLong.Where(x => x.Item1 == 2);
+                    if(lBuy1.Any())
                     {
-                        var indicator = item.indicator.FirstOrDefault(x => x.ty == (int)EIndicator.SuperTrend);
-                        sBuilder.AppendLine($"{item.s}(TP trung bình: {indicator.avg}%|Nam giu: {indicator.num}| Win/Loss: {indicator.win}%/{indicator.loss}%)");
+                        sBuilder.AppendLine("+ Mua vượt đỉnh:");
+                        foreach (var item in lBuy1)
+                        {
+                            sBuilder.AppendLine($"   - {item.Item2}");
+                        }
                     }
-                }
-
-                if (lSuperTrend.Any())
-                {
-                    sBuilder.AppendLine();
-                    sBuilder.AppendLine("[Tín hiệu SuperTrend - COIN]");
-
-                    foreach (var item in lSuperTrend.Take(10))
+                    if (lBuy2.Any())
                     {
-                        var indicator = item.indicator.FirstOrDefault(x => x.ty == (int)EIndicator.SuperTrend);
-                        sBuilder.AppendLine($"{item.s}(TP trung bình: {indicator.avg}%|Nam giu: {indicator.num}| Win/Loss: {indicator.win}%/{indicator.loss}%)");
-                    }
-                }
-
-                if (lSuperTrendPhrase2.Any())
-                {
-                    sBuilder.AppendLine();
-                    sBuilder.AppendLine("[Tín hiệu SuperTrend - Phrase 2 - COIN]");
-
-                    foreach (var item in lSuperTrendPhrase2.Take(10))
-                    {
-                        var indicator = item.indicator.FirstOrDefault(x => x.ty == (int)EIndicator.SuperTrendPhrase2);
-                        sBuilder.AppendLine($"{item.s}(TP trung bình: {indicator.avg}%|Nam giu: {indicator.num}| Win/Loss: {indicator.win}%/{indicator.loss}%)");
+                        sBuilder.AppendLine("+ Mua Test lại:");
+                        foreach (var item in lBuy2)
+                        {
+                            sBuilder.AppendLine($"   - {item.Item2}");
+                        }
                     }
                 }
 
@@ -116,5 +67,114 @@ namespace StockLib.Service
 
             return (0, null);
         }
+
+        //public async Task<(int, string)> TinHieuMuaBanCoin()
+        //{
+        //    try
+        //    {
+        //        var lCoin = _coinRepo.GetAll();
+
+        //        var lDanZVolume = new List<Coin>();
+        //        var lSuperTrend = new List<Coin>();
+        //        var lSuperTrendPhrase2 = new List<Coin>();
+
+        //        foreach (var item in lCoin)
+        //        {
+        //            if (item is null
+        //                || !item.indicator.Any())
+        //                continue;
+
+        //            var lByBit = await StaticVal.ByBitInstance().V5Api.ExchangeData.GetKlinesAsync(Bybit.Net.Enums.Category.Spot, item.s, Bybit.Net.Enums.KlineInterval.OneHour, null, null, 1000);
+        //            var lData = lByBit.Data.List.Select(x => new Quote
+        //            {
+        //                Date = x.StartTime,
+        //                Open = x.OpenPrice,
+        //                High = x.HighPrice,
+        //                Low = x.LowPrice,
+        //                Close = x.ClosePrice,
+        //                Volume = x.Volume,
+        //            }).ToList();
+
+        //            if (item.indicator.Any(x => x.ty == (int)EIndicator.DanZangerVolumne))
+        //            {
+        //                var isDanz = lData.CheckDanZangerCustom(7);
+        //                if (isDanz)
+        //                {
+        //                    lDanZVolume.Add(item);
+        //                }
+        //            }
+
+        //            if (item.indicator.Any(x => x.ty == (int)EIndicator.SuperTrend))
+        //            {
+        //                var isSuperTrend = lData.CheckSuperTrend();
+        //                if (isSuperTrend)
+        //                {
+        //                    lSuperTrend.Add(item);
+        //                }
+        //            }
+
+
+        //            if (item.indicator.Any(x => x.ty == (int)EIndicator.SuperTrendPhrase2))
+        //            {
+        //                var isSuperTrendPhrase2 = lData.CheckSuperTrendPharse2();
+        //                if (isSuperTrendPhrase2)
+        //                {
+        //                    lSuperTrendPhrase2.Add(item);
+        //                }
+        //            }
+        //        }
+
+        //        var sBuilder = new StringBuilder();
+
+
+        //        if (lDanZVolume.Any())
+        //        {
+        //            sBuilder.AppendLine();
+        //            sBuilder.AppendLine("[Tín hiệu DanZVolume - COIN]");
+
+        //            foreach (var item in lDanZVolume.Take(10))
+        //            {
+        //                var indicator = item.indicator.FirstOrDefault(x => x.ty == (int)EIndicator.SuperTrend);
+        //                sBuilder.AppendLine($"{item.s}(TP trung bình: {indicator.avg}%|Nam giu: {indicator.num}| Win/Loss: {indicator.win}%/{indicator.loss}%)");
+        //            }
+        //        }
+
+        //        if (lSuperTrend.Any())
+        //        {
+        //            sBuilder.AppendLine();
+        //            sBuilder.AppendLine("[Tín hiệu SuperTrend - COIN]");
+
+        //            foreach (var item in lSuperTrend.Take(10))
+        //            {
+        //                var indicator = item.indicator.FirstOrDefault(x => x.ty == (int)EIndicator.SuperTrend);
+        //                sBuilder.AppendLine($"{item.s}(TP trung bình: {indicator.avg}%|Nam giu: {indicator.num}| Win/Loss: {indicator.win}%/{indicator.loss}%)");
+        //            }
+        //        }
+
+        //        if (lSuperTrendPhrase2.Any())
+        //        {
+        //            sBuilder.AppendLine();
+        //            sBuilder.AppendLine("[Tín hiệu SuperTrend - Phrase 2 - COIN]");
+
+        //            foreach (var item in lSuperTrendPhrase2.Take(10))
+        //            {
+        //                var indicator = item.indicator.FirstOrDefault(x => x.ty == (int)EIndicator.SuperTrendPhrase2);
+        //                sBuilder.AppendLine($"{item.s}(TP trung bình: {indicator.avg}%|Nam giu: {indicator.num}| Win/Loss: {indicator.win}%/{indicator.loss}%)");
+        //            }
+        //        }
+
+        //        if (string.IsNullOrWhiteSpace(sBuilder.ToString()))
+        //        {
+        //            return (0, null);
+        //        }
+        //        return (1, sBuilder.ToString());
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError($"AnalyzeService.TinHieuMuaBanCoin|EXCEPTION| {ex.Message}");
+        //    }
+
+        //    return (0, null);
+        //}
     }
 }
