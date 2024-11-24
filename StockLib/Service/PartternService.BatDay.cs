0e50da2e5fcf25b2570df31477d55b8a7bf6f267
Nguyễn Văn Phú <asutaka@outlook.com>
2024-11-25 00:03:28 +0700

    update code

--- StockLib/Service/PartternService.BatDay.cs
+++ StockLib/Service/PartternService.BatDay.cs
@@ -11,7 +11,7 @@ namespace StockLib.Service
             try
             {
                 _code = code;
-                var lByBit = await StaticVal.ByBitInstance().V5Api.ExchangeData.GetKlinesAsync(Bybit.Net.Enums.Category.Linear, code, Bybit.Net.Enums.KlineInterval.OneHour, null, null, 1000);
+                var lByBit = await StaticVal.ByBitInstance().V5Api.ExchangeData.GetKlinesAsync(Bybit.Net.Enums.Category.Linear, code, Bybit.Net.Enums.KlineInterval.FourHours, null, null, 1000);
                 var lData = lByBit.Data.List.Select(x => new Quote
                 {
                     Date = x.StartTime,
@@ -59,77 +59,78 @@ namespace StockLib.Service
                     var bb = lbb.ElementAt(i);
                     var itemVol = lMaVol.ElementAt(i);
 
-                    if (isHold)
-                    {
-                        var rate = Math.Round(100 * (-1 + item.Close / entryHold), 1);
-                        if (i - indexHold >= 20) 
-                        {
-                            var tp = Math.Round(100 * (-1 + item.Close / itemHold.Close), 1);
-                            Console.WriteLine($"BUY: {itemHold.Date.ToString($"dd/MM/yyyy HH")}|SELL: {item.Date.ToString($"dd/MM/yyyy HH")}| TP: {tp}");
-                            itemHold = null;
-                            entryHold = 0;
-                            isHold = false;
-                            indexHold = -1;
-                        }
-                        if(item.Low <= sl)
-                        {
-                            var tp = Math.Round(100 * (-1 + sl / itemHold.Close), 1);
-                            Console.WriteLine($"BUY: {itemHold.Date.ToString($"dd/MM/yyyy HH")}|SELL: {item.Date.ToString($"dd/MM/yyyy HH")}| TP: {tp}");
-                            itemHold = null;
-                            entryHold = 0;
-                            isHold = false;
-                            indexHold = -1;
-                        }
-                        if(item.Close < item.Open && item.Volume > (decimal)(itemVol.Sma * 1.5))
-                        {
-                            var tp = Math.Round(100 * (-1 + item.Close / itemHold.Close), 1);
-                            Console.WriteLine($"BUY: {itemHold.Date.ToString($"dd/MM/yyyy HH")}|SELL: {item.Date.ToString($"dd/MM/yyyy HH")}| TP: {tp}");
-                            itemHold = null;
-                            entryHold = 0;
-                            isHold = false;
-                            indexHold = -1;
-                        }
+                    //if (isHold)
+                    //{
+                    //    var rate = Math.Round(100 * (-1 + item.Close / entryHold), 1);
+                    //    if (i - indexHold >= 20) 
+                    //    {
+                    //        var tp = Math.Round(100 * (-1 + item.Close / itemHold.Close), 1);
+                    //        Console.WriteLine($"BUY: {itemHold.Date.ToString($"dd/MM/yyyy HH")}|SELL: {item.Date.ToString($"dd/MM/yyyy HH")}| TP: {tp}");
+                    //        itemHold = null;
+                    //        entryHold = 0;
+                    //        isHold = false;
+                    //        indexHold = -1;
+                    //    }
+                    //    if(item.Low <= sl)
+                    //    {
+                    //        var tp = Math.Round(100 * (-1 + sl / itemHold.Close), 1);
+                    //        Console.WriteLine($"BUY: {itemHold.Date.ToString($"dd/MM/yyyy HH")}|SELL: {item.Date.ToString($"dd/MM/yyyy HH")}| TP: {tp}");
+                    //        itemHold = null;
+                    //        entryHold = 0;
+                    //        isHold = false;
+                    //        indexHold = -1;
+                    //    }
+                    //    if(item.Close < item.Open && item.Volume > (decimal)(itemVol.Sma * 1.5))
+                    //    {
+                    //        var tp = Math.Round(100 * (-1 + item.Close / itemHold.Close), 1);
+                    //        Console.WriteLine($"BUY: {itemHold.Date.ToString($"dd/MM/yyyy HH")}|SELL: {item.Date.ToString($"dd/MM/yyyy HH")}| TP: {tp}");
+                    //        itemHold = null;
+                    //        entryHold = 0;
+                    //        isHold = false;
+                    //        indexHold = -1;
+                    //    }
 
-                        if(rate >= 10)
-                        {
-                            var real = 1 - Math.Ceiling(rate / 2) / 100;
-                            var slcur = item.High * real;
-                            if(slcur > sl)
-                            {
-                                sl = slcur;
-                            }
-                        }
+                    //    if(rate >= 10)
+                    //    {
+                    //        var real = 1 - Math.Ceiling(rate / 2) / 100;
+                    //        var slcur = item.High * real;
+                    //        if(slcur > sl)
+                    //        {
+                    //            sl = slcur;
+                    //        }
+                    //    }
 
-                        continue;
-                    }
+                    //    continue;
+                    //}
 
-                    if(isBuy)
-                    {
-                        isBuy = false;
-                        var entry = (decimal)0.6 * itemFlag.Close + (decimal)0.4 * itemFlag.Open;
-                        if(item.Low <= entry)
-                        {
-                            itemHold = item;
-                            entryHold = entry;
-                            isHold = true;
-                            indexHold = i;
-                            sl = entry * (decimal)0.7;
-                            //Console.WriteLine($"{item.Date.ToString($"dd/MM/yyyy HH")}|Val: {entry}");
-                        }
-                        itemFlag = null;
-                        continue;
-                    }
+                    //if(isBuy)
+                    //{
+                    //    isBuy = false;
+                    //    var entry = (decimal)0.6 * itemFlag.Close + (decimal)0.4 * itemFlag.Open;
+                    //    if(item.Low <= entry)
+                    //    {
+                    //        itemHold = item;
+                    //        entryHold = entry;
+                    //        isHold = true;
+                    //        indexHold = i;
+                    //        sl = entry * (decimal)0.7;
+                    //        //Console.WriteLine($"{item.Date.ToString($"dd/MM/yyyy HH")}|Val: {entry}");
+                    //    }
+                    //    itemFlag = null;
+                    //    continue;
+                    //}
 
                     if (item.Close < item.Open * (decimal)1.01
-                        || item.Volume <= (decimal)(itemVol.Sma * 1.5)
+                        //|| item.Volume <= (decimal)(itemVol.Sma * 1.5)
                         || item.Low >= (decimal)bb.LowerBand
                         || item.Close >= (decimal)bb.Sma
-                        || (item.High - item.Low) * 2 > (decimal)(bb.UpperBand - bb.LowerBand)) 
+                        //|| (item.High - item.Low) * 2 > (decimal)(bb.UpperBand - bb.LowerBand)
+                        ) 
                         continue;
                     isBuy = true;
                     itemFlag = item;
 
-                    //Console.WriteLine($"{item.Date.ToString($"dd/MM/yyyy HH")}|Vol: {item.Volume}|MA: {itemVol.Sma}");
+                    Console.WriteLine($"{item.Date.ToString($"dd/MM/yyyy HH")}|Vol: {item.Volume}|MA: {itemVol.Sma}");
 
                     //var buy = (3 * item.High + item.Low) / 4;
                     //if(item.Open - item.Low >= 4*(item.High - item.Close))
